31de4c1da1c26d49b0f7253dd27f2f57d1c6f816
Suhaib200119 <sis.22.18a@gmail.com>
2023-07-19 00:00:37 +0300

    first commit

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UploadFileRequset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class UploadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+    return view("home");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UploadFileRequset $request)
+    {
+     $validation=$request->validated();
+     if($request->hasFile("userFile")){
+        $file=$request->file("userFile");
+        $path = $file->store("/files","uploads");
+        $link = asset('uploads/' . $path);
+        Session::flash("data", $link);
+     }else{
+        Session::flash("data","no files!");
+     }
+     return redirect()->route("uploadFile.create");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Share Files</title>
+  </head>
+  <body>
+ <div class="position-absolute top-50 start-50 translate-middle">
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @if (session()->has("data"))
+        <div class="alert aler-success">
+            {{session("data")}}
+        </div>
+    @endif
+<h2 class="text-success">Share your files by link</h2>
+    <form action="{{route("uploadFile.store")}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="formFile" class="form-label">Upload Your File</label>
+            <input class="form-control" type="file" id="formFile" name="userFile">
+            @error("userFile")
+                <p class="text-danger">{{$message}}</p>
+            @enderror
+          </div>
+
+          <button type="submit" class="btn btn-primary" style="width: 100%">Get Link</button>
+    </form>
+ </div>
+
+   
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
